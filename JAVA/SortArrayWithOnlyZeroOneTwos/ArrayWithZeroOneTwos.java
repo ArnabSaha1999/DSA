2a25afd271649eeafe39d2e175cd1365deeef6cf
ArnabSaha1999 <83657302+ArnabSaha1999@users.noreply.github.com>
2021-08-01 10:32:29 +0530

    Add files via upload

---
+++ JAVA/SortArrayWithOnlyZeroOneTwos/ArrayWithZeroOneTwos.java
@@ -0,0 +1,32 @@
+package Arnab.com;
+
+public class ArrayWithZeroOneTwos {
+    public void sort012(int[] a , int n) {
+        int mid = 0 , low = 0 , temp = 0;
+        int high = n - 1;
+        while(mid <= high) {
+            switch(a[mid]) {
+                case 0 :
+                    temp = a[low];
+                    a[low] = a[mid];
+                    a[mid] = temp;
+                    mid++;
+                    low++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    temp = a[high];
+                    a[high] = a[mid];
+                    a[mid] = temp;
+                    high--;
+                default:
+                    break;
+            }
+        }
+        for(int i:a){
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ JAVA/SortArrayWithOnlyZeroOneTwos/Main.java
@@ -0,0 +1,10 @@
+package Arnab.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ArrayWithZeroOneTwos array = new ArrayWithZeroOneTwos();
+	    int[] arr = {1,2,1,2,0,2,1,2,0};
+	    array.sort012(arr , arr.length);
+    }
+}
