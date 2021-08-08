a7e6f650c1b623b20f1034eaf95e0d64a70a5c81
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2021-08-08 11:13:10 +0530

    Create MergeSort.java

---
+++ JAVA/MergeSort/MergeSort.java
@@ -0,0 +1,48 @@
+package Arnab.com;
+
+class MergeSort {
+    public void mergeSort(int[] arr , int left , int right) {
+        if(left < right) {
+            int mid = (left + right) / 2 ;
+            mergeSort(arr , left , mid);
+            mergeSort(arr , mid+1 , right);
+            merge(arr , left , mid , right);
+        }
+    }
+
+    public void merge(int[] arr , int left , int mid , int right){
+        int[] mergedArray = new int[arr.length];
+        int i = left , j = mid+1 , k = left;
+        while(i <= mid && j <= right) {
+            if(arr[i] < arr[j]) {
+                mergedArray[k] = arr[i];
+                i++;
+            } else {
+                mergedArray[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(j <= right) {
+            mergedArray[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        while (i <= mid) {
+            mergedArray[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        for(int it = left ; it <= right ; it++) {
+            arr[it] = mergedArray[it];
+        }
+    }
+    public void printArray(int[] arr) {
+        for(int i:arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
