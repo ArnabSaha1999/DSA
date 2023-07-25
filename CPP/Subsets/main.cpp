0d074b3f0e8877fb6203b754da9ca94f1b73cf55
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-07-25 12:24:18 +0530

    Update main.cpp

--- CPP/Subsets/main.cpp
+++ CPP/Subsets/main.cpp
@@ -22,29 +22,64 @@ void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>&
     
 }
 
-void findPowerSet(vector<int>& nums, vector<int>& output, int index, vector<vector<int>>& ans) {
-    ans.push_back(output);
-    
-    for (int i = index; i < nums.size(); ++i) {
-        output.push_back(nums[i]);
-        findPowerSet(nums, output, i + 1, ans);
-        output.pop_back();
-    }
-}
 
+//Approach 1: Function to find all subsets using backtracking
 vector<vector<int>> subsets(vector<int>& nums) {
     vector<vector<int>> ans;
     vector<int> output;
-    
     int index = 0;
-    
-    findPowerSet(nums, output, index, ans);
+    solve(nums, output, index, ans);
     return ans;
     
 }
 
+//Approach 2: Function to find the power set using the Bitwise approach 
+vector<vector<int>> subsetsBitwise(vector<int> &nums) {
+    int totalSubsets = 1 << nums.size();
+    vector<vector<int>> ans;
+    for(int i = 0; i < totalSubsets; i++) {
+        vector<int> subsets;
+        for(int j = 0; j < nums.size(); j++) {
+            // If the j-th bit of i is set, add the corresponding element to the subset
+            if(i & (1 << j)) {
+                subsets.push_back(nums[j]);
+            }
+        }
+        ans.push_back(subsets); // Store the current subset in the power set
+    }
+    return ans;
+
+}
+
+// Function to print an array
+void printArray(vector<int> &arr) {
+    cout<<"{";
+    for(int i = 0; i < arr.size(); i++) {
+        cout<<arr[i];
+        if(i != arr.size() - 1) {
+            cout<<",";
+        }
+    }
+    
+    cout<<"}";
+}
+
 int main() {
     vector<int> arr = {1,2,3};
+
+    vector<vector<int>> ans_backtracking = subsets(arr);
+    cout<<"The Subsets of the array (Backtracking approach): "<<endl<<"{";
+    for (auto &i : ans_backtracking) {
+        printArray(i);
+    }
+    cout<<"}"<<endl;
+
     vector<vector<int>> ans = subsets(arr);
+    cout<<"The Subsets of the array: "<<endl<<"{";
+    for(auto &i : ans) {
+        printArray(i);
+    }
+    cout<<"}"<<endl;
+    
     return 0;
 }
