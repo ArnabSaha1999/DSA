a134d83baf2b3f3e532d8e5c861b008f2db7e66c
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-08-17 11:30:07 +0530

    Update main.cpp

--- CPP/LinkedList/DetectLoopInLinkedList/main.cpp
+++ CPP/LinkedList/DetectLoopInLinkedList/main.cpp
@@ -14,7 +14,7 @@ public:
     } 
 };
 
-// Approach: Hash Set (unordered_map) for Cycle Detection
+// Approach 1: Hash Set (unordered_map) for Cycle Detection
 // Time Complexity: O(n), where n is the number of nodes in the linked list
 // Space Complexity: O(n), a hash map is used to store visited nodes
 bool hasCycleHashMap(Node* &head) {
