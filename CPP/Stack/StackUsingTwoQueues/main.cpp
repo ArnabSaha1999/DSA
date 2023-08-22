ea9992fa1c5f14ac2f3d9c84772e134b4f17a97a
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-08-22 23:10:53 +0530

    Create main.cpp

---
+++ CPP/Stack/StackUsingTwoQueues/main.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+// A Stack class with two queues
+class Stack{
+    queue<int> q1, q2;
+public:
+    // Pushes an element onto the stack
+    void push(int value) {
+        q2.push(value);
+        // Move elements from q1 to q2, and swap the queues
+        while(!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+        q2.empty();
+    }
+    
+    // Removes and returns the top element from the stack
+    int pop() {
+        // Stack is empty
+        if(q1.empty()) {
+            return -1;
+        }
+        int deletedTop = q1.front();
+        q1.pop();
+        return deletedTop;
+    }
+
+    // Returns the top element of stack
+    int getTop() {
+        // Stack is empty
+        if(q1.empty()) {
+            return -1;
+        }
+        return q1.front();
+        
+    }
+
+    // Checks if the stack is empty
+    bool isEmpty() {
+        if(q1.empty()) {
+            return true;
+        }
+        return false;
+    }
+
+    // Returns the size of the stack
+    int getSize() {
+        if(q1.empty()) {
+            return 0;
+        }
+        return q1.size();
+    }
+
+    // Destructor to release memory used by queues
+    ~Stack() {
+        while (!q1.empty()) {
+            q1.pop();
+        }
+    }
+};
+
+int main() {
+    Stack st;
+
+    if(!st.isEmpty()) {
+        cout<<"The Stack is empty"<<endl;
+    } else {
+        cout<<"The Stack is not empty"<<endl;
+    }
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+
+    cout<<"The Top of Stack: "<<st.getTop()<<endl;
+    cout<<"The Size of Stack: "<<st.getSize()<<endl;
+    cout<<"The Popped element from Stack: "<<st.pop()<<endl;
+
+    cout<<"The Top of Stack: "<<st.getTop()<<endl;
+    cout<<"The Size of Stack: "<<st.getSize()<<endl;
+
+    st.push(7);
+
+    cout<<"The Top of Stack: "<<st.getTop()<<endl;
+    cout<<"The Size of Stack: "<<st.getSize()<<endl;
+
+    return 0;
+}
