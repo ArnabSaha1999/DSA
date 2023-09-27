7624fcea0da691a1566632e22a52cf869b5a28cd
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-09-27 17:47:22 +0530

    Create main.cpp

---
+++ CPP/BinaryTree/AverageLevelBinaryTree/main.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+// Define a class for binary tree nodes
+class Node {
+public:
+    Node* left;
+    Node* right;
+    int value;
+    // Constructor to initialize a node with a given value
+    Node(int value) {
+        this->value = value;
+        this->left = this->right = nullptr;
+    }
+};
+
+// Public function to perform level-order traversal
+void levelOrderTraversal(Node* root) {
+    if(!root) {
+        cout<<"Binary Tree is empty"<<endl;
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    int level = 0;
+    // Begin the level-order traversal
+    while(!q.empty()) {
+        // Get the number of nodes at the current level
+        int size = q.size();
+        // Print the current level
+        cout<<"Level "<<level++<<": ";
+        // Process nodes at the current level
+        for(int i = 0; i < size; i++) {
+            // Get the front node from the queue
+            Node* temp = q.front();
+            // Remove the front node from the queue
+            q.pop();
+            // Enqueue the left child if it exists
+            if(temp->left) {
+                q.push(temp->left);
+            }
+            // Enqueue the right child if it exists
+            if(temp->right) {
+                q.push(temp->right);
+            }
+            cout<<temp->value<<" "; // Print the value of the current node
+        }
+        // Move to the next level (line break)
+        cout<<endl;
+    }
+}
+
+//Approach 1: Function to calculate the average values of levels in a binary tree
+vector<double> averageOfLevels(Node* root) {
+    if(!root) {
+        // If the tree is empty, return an empty vector
+        return {};
+    }
+    queue<Node*> q;
+    q.push(root);
+    double average;
+    vector<double> ans; // Vector to store average values
+    while(!q.empty()) {
+        // Get the number of nodes at the current level
+        int size = q.size();
+        // Initialize the sum for the current level
+        double sum = 0.0;
+        for(int i = 0; i < size; i++) {
+            Node* currNode = q.front();
+            q.pop();
+            // Add the value of the current node to the sum
+            sum += currNode->value;
+            // Enqueue the left child if it exists
+            if(currNode->left) {
+                q.push(currNode->left);
+            }
+            // Enqueue the right child if it exists
+            if(currNode->right) {
+                q.push(currNode->right);
+            }
+        }
+        // Calculate the average for the current level
+        average = sum / size;
+        // Add the average to the result vector
+        ans.push_back(average);
+    }
+    // Return the vector of average values
+    return ans;
+}
+
+// Function to delete the binary tree to free memory
+void deleteTree(Node* root) {
+    if (!root) {
+        return;
+    }
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+}
+
+int main() {
+    Node* root;
+    root = new Node(5);
+    root->left = new Node(3);
+    root->left->left = new Node(11);
+    root->left->left->left = new Node(9);
+    root->left->left->right = new Node(12);
+    root->left->right = new Node(1);
+    root->right = new Node(7);
+    root->right->right = new Node(6);
+    root->right->right->left = new Node(13);
+    root->right->right->right = new Node(15);
+
+    cout<<"The Level Order Traversal of Binary Tree: "<<endl;
+    levelOrderTraversal(root);
+
+    cout<<"The Average of Levels: "<<endl;
+    vector<double> ans = averageOfLevels(root);
+    for(double a : ans) {
+        cout<<a<<" ";
+    } cout<<endl;
+
+    // Clean up memory by deleting the nodes
+    deleteTree(root);
+
+    return 0;
+
+}
