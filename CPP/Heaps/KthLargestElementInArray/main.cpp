80311586af0e56d3f0d30268a459e42ccd4cc2df
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-11-01 16:45:22 +0530

    Update main.cpp

--- CPP/Heaps/KthLargestElementInArray/main.cpp
+++ CPP/Heaps/KthLargestElementInArray/main.cpp
@@ -31,7 +31,7 @@ int findKthLargestElementMaxHeap(vector<int> &arr, int k) {
         maxHeap.push(i);
     }
     // Remove the k - 1 largest elements
-    while(k-- != 1) {
+    while(--k != 0) {
         maxHeap.pop();
     }
     // Return the kth largest element
@@ -58,6 +58,31 @@ int findKthLargestElementMinHeap(vector<int> &arr, int k) {
     return minHeap.top();
 }
 
+//Approach 4: Function to find the kth largest element in an array using a min heap (Optimized Approach)
+int findKthLargestElementOptimized(vector<int> &arr, int k) {
+    int size = arr.size();
+    // Check if the value of k is out of range or invalid
+    if(k <= 0 || k > size) {
+        return -1;
+    }
+    // Create a min heap to maintain the k largest elements
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for(int i = 0; i < k; i++) {
+        minHeap.push(arr[i]);
+    }
+    // Insert the first k elements into the min heap
+    for(int i = k; i < arr.size(); i++) {
+        // If the current element is greater than the current minimum in the heap
+        if(arr[i] > minHeap.top()) {
+            // Replace the current minimum with the larger element
+            minHeap.pop();
+            minHeap.push(arr[i]);
+        }
+    }
+    // The top element of the min heap is the kth largest element
+    return minHeap.top();
+}
+
 // Function to print the elements of an array
 void printArray(vector<int> arr) {
     cout<<"The Array Elements: "<<endl;
@@ -73,6 +98,7 @@ int main() {
     cout<<endl<<"The "<<k<<" largest element using Brute Force Approach: "<<findKthLargestElementBruteForce(arr, k)<<endl;
     cout<<endl<<"The "<<k<<" largest element using Max Heap Approach: "<<findKthLargestElementMaxHeap(arr, k)<<endl;
     cout<<endl<<"The "<<k<<" largest element using Min Heap Approach: "<<findKthLargestElementMinHeap(arr, k)<<endl;
+    cout<<endl<<"The "<<k<<" largest element using Optimized Approach: "<<findKthLargestElementOptimized(arr, k)<<endl;
 
     return 0;
 }
