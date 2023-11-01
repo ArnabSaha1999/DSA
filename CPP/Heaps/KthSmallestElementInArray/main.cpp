d49a5e481cb2e1620826202b3cdf3451cbb9d11d
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2023-11-01 16:40:43 +0530

    Update main.cpp

--- CPP/Heaps/KthSmallestElementInArray/main.cpp
+++ CPP/Heaps/KthSmallestElementInArray/main.cpp
@@ -51,11 +51,37 @@ int kthSmallestElementMinHeap(vector<int> &arr, int k) {
         minHeap.push(i);
     }
     // Remove elements from the min heap until the kth smallest element is found
-    while(k-- != 1) {
+    while(--k != 0) {
         minHeap.pop();
     }
     // Return the kth smallest element
     return minHeap.top();
+}
+
+//Approach 4: Function to find the kth smallest element in the array using a max heap (Optimized Approach)
+int kthSmallestElementOptimized(vector<int> &arr, int k) {
+    int size = arr.size();
+    // Check if the value of k is out of range or invalid
+    if(k <= 0 || k > size) {
+        return -1;
+    }
+    // Create a max heap to maintain the k smallest elements
+    priority_queue<int> maxHeap;
+    // Insert the first k elements into the max heap
+    for(int i = 0; i < k; i++) {
+        maxHeap.push(arr[i]);
+    }
+    // Iterate through the remaining elements in the array
+    for(int i = k; i < arr.size(); i++) {
+        // If the current element is smaller than the current maximum in the heap
+        if(arr[i] < maxHeap.top()) {
+            // Replace the current maximum with the smaller element
+            maxHeap.pop();
+            maxHeap.push(arr[i]);
+        }
+    }
+    // The top element of the max heap is the kth smallest element
+    return maxHeap.top();
 }
 
 // Function to print the elements of an array
@@ -68,11 +94,12 @@ void printArray(vector<int> arr) {
 
 int main() {
     vector<int> arr = {3,2,1,5,6,4};
-    int k = 2;
+    int k = 3;
     printArray(arr);
     cout<<endl<<"The "<<k<<" smallest element using Brute Force Approach: "<<kthSmallestElementBruteForce(arr, k)<<endl;
     cout<<endl<<"The "<<k<<" smallest element using Max Heap Approach: "<<kthSmallestElementMaxHeap(arr, k)<<endl;
     cout<<endl<<"The "<<k<<" smallest element using Min Heap Approach: "<<kthSmallestElementMinHeap(arr, k)<<endl;
+    cout<<endl<<"The "<<k<<" smallest element using the Optimized Approach: "<<kthSmallestElementOptimized(arr, k)<<endl;
 
     return 0;
 }
